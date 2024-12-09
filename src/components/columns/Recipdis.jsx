d407865cef787737d3d3bb708fc8782de505cebe
dann1m <dannieim689@gmail.com>
2024-12-08 22:44:05 -0500

    Hero image path updated

--- src/components/columns/Recipdis.jsx
+++ src/components/columns/Recipdis.jsx
@@ -6,7 +6,7 @@ const Recipdis = () => {
             <div className="dis-content">
                 <h1 className="dis-title">Delicious Recipes, Crafted for Every Diet</h1>
                 <div className="dis-image">
-                    <img src='src/assets/healthy foods.jpg'></img>
+                    <img className='dis-image' src='src/assets/healthy foods.jpg'></img>
                 </div>
                 <p className="dis-subtitle">Explore meals that cater to your needs without compromise.</p>
                 <button className="dis-button">Find Yours</button>
